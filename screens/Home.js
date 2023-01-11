ca256f0da937c81acef180de0482ff6407118096
Zarivate <Zarivate1@gmail.com>
2023-01-11 00:00:00 -0600

    API retreival trouble

--- screens/Home.js
+++ screens/Home.js
@@ -13,6 +13,17 @@ const YELP_API_KEY = process.env.YELP_API_KEY;
 export default function Home() {
   const [restaurantData, setRestaurantData] = useState(restaurantSamples);
 
+  const getRestaurantsYelp = () => {
+    const yelpUrl =
+      "https://api.yelp.com/v3/businesses/search?term=restaurants&location=SanDiego";
+  };
+
+  const apiOptions = {
+    headers: {
+      Authorization: `Bearer ${YELP_API_KEY}`,
+    },
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#eee" }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
