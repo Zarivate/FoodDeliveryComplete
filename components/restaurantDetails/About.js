20627b5705c683eb316032562cddd24b1486f013
Zarivate <Zarivate1@gmail.com>
2023-01-13 23:59:50 -0600

    Starting new component

---
+++ components/restaurantDetails/About.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const image =
+  "https://www.beyondpinkworld.com/Assets/News/2855/BP_1_636898382689406962.jpg";
+
+const title = "Toilet Restaurant";
+const description =
+  "Do not be fooled, food is actually delicious at unbelivably low prices";
+
+export default function About() {
+  return (
+    <View>
+      <Text>About</Text>
+    </View>
+  );
+}
--- screens/RestaurantDetails.js
+++ screens/RestaurantDetails.js
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Divider } from "react-native-elements";
+import About from "../components/restaurantDetails/About";
 
 export default function RestaurantDetails() {
   return (
     <View>
-      <Text>RestaurantDetails</Text>
+      <About />
+      <Divider width={1.8} style={{ marginVertical: 20 }} />
     </View>
   );
 }
