20d0f6da2171b1cbc4446d4eeb67447538e82cda
Zarivate <Zarivate1@gmail.com>
2023-01-26 14:59:54 -0600

    Added styling to completion screen

--- components/restaurantDetails/MenuItems.js
+++ components/restaurantDetails/MenuItems.js
@@ -5,39 +5,12 @@ import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 
-const testFoods = [
-  {
-    title: "Big Grapes",
-    description: "What more needs to be said? Ir's grapes but for the big bois",
-    price: "$7.77",
-    image: "https://m.media-amazon.com/images/I/71949a+B2YL._SL1500_.jpg",
-  },
-  {
-    title: "test2",
-    description: "Literally one of the best ways to die that's under $20",
-    price: "$12.57",
-    image:
-      "https://okamotokitchen.com/wprs/wp-content/uploads/2016/09/SnS_Screenshot2.jpg",
-  },
-  {
-    title: "Chicken n Waffles",
-    description:
-      "Waffles and chicken friend and seasoned too perfection with a healthy dose of syrup on the side",
-    price: "$6.53",
-    image:
-      "http://cdn.shopify.com/s/files/1/0579/9362/5765/articles/20211102155531-fried-chicken-tender-and-caramel-pecan-waffles-1024x685.jpg?v=1635869756",
-  },
-  {
-    title: "Heart Attack Sandwich",
-    description:
-      "Just look at it, it'll taste good on the way down but your blood sugar will hate you",
-    price: "$7.50",
-    image:
-      "https://www.thesun.co.uk/wp-content/uploads/2020/04/NINTCHDBPICT000577254125.jpg",
-  },
-];
-
-export default function MenuItems({ restaurantName }) {
+export default function MenuItems({
+  restaurantName,
+  testFoods,
+  hideCheckbox,
+  marginLeft,
+}) {
   const dispatch = useDispatch();
 
   // Runs everytime a user selects an item, dispatches item selected
@@ -63,17 +36,22 @@ export default function MenuItems({ restaurantName }) {
       {testFoods.map((food, index) => (
         <View key={index}>
           <View style={styles.menuItemStyle}>
-            <BouncyCheckbox
-              size={18}
-              iconStyle={{ borderRadius: 0 }}
-              innerIconStyle={{ borderRadius: 0, borderColor: "lightgray" }}
-              fillColor="green"
-              // The checkboxValue prop from bouncyCheckbox will be used to decided when too add or remove an item from a user's cart
-              onPress={(checkboxValue) => selectItem(food, checkboxValue)}
-              isChecked={isInCart(food, cartItems)}
-            />
+            {/* Checks to see if checkboxes are hidden so that any data on the OrderComplete screen doesn't have the checkboxes */}
+            {hideCheckbox ? (
+              <></>
+            ) : (
+              <BouncyCheckbox
+                size={18}
+                iconStyle={{ borderRadius: 0 }}
+                innerIconStyle={{ borderRadius: 0, borderColor: "lightgray" }}
+                fillColor="green"
+                // The checkboxValue prop from bouncyCheckbox will be used to decided when too add or remove an item from a user's cart
+                onPress={(checkboxValue) => selectItem(food, checkboxValue)}
+                isChecked={isInCart(food, cartItems)}
+              />
+            )}
             <FoodDetails food={food} />
-            <FoodImage food={food} />
+            <FoodImage food={food} marginLeft={marginLeft ? marginLeft : 0} />
           </View>
           <Divider
             width={0.5}
@@ -94,7 +72,7 @@ const FoodDetails = (props) => (
   </View>
 );
 
-const FoodImage = (props) => (
+const FoodImage = ({ marginLeft, ...props }) => (
   <View>
     <Image
       source={{ uri: props.food.image }}
@@ -102,6 +80,7 @@ const FoodImage = (props) => (
         width: 75,
         height: 75,
         borderRadius: 8,
+        marginLeft: marginLeft,
       }}
     />
   </View>
--- screens/OrderComplete.js
+++ screens/OrderComplete.js
@@ -1,10 +1,25 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { SafeAreaView } from "react-native-safe-area-context";
 import LottieView from "lottie-react-native";
+import firebase from "firebase/compat";
+import MenuItems from "../components/restaurantDetails/MenuItems";
+import { ScrollView } from "react-native";
 
 export default function OrderComplete() {
+  const [lastOrder, setLastOrder] = useState({
+    items: [
+      {
+        title: "Test",
+        description:
+          "A blank order meant for testing out whether data can be correctly retrieved and overwrite this dummy text. If you're reading this that means it failed.",
+        price: "$5.15",
+        image:
+          "https://irepo.primecp.com/2022/12/544963/Strawberry-French-Toast-Roll-Ups-8131-H_ExtraLarge800_ID-5038791.jpg?v=5038791",
+      },
+    ],
+  });
   const { items, restaurantName } = useSelector(
     (state) => state.cartReducer.selectedItems
   );
@@ -18,24 +33,48 @@ export default function OrderComplete() {
     currency: "USD",
   });
 
+  useEffect(() => {
+    const db = firebase.firestore();
+    const unsubscribe = db
+      .collection("orders")
+      .orderBy("createdAt", "desc")
+      .limit(1)
+      .onSnapshot((snapshot) => {
+        snapshot.docs.map((doc) => {
+          setLastOrder(doc.data());
+        });
+      });
+
+    // Once useEffect is finished, connection to firebase database is shutdown. Make anonymous call to unsubscribe.
+    return () => unsubscribe();
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-      <LottieView
-        style={{ height: 100, alignSelf: "center", marginBottom: 30 }}
-        source={require("../assets/animations/check-mark.json")}
-        autoPlay
-        speed={0.5}
-        loop={true}
-      />
-      <Text>
-        Your order for {restaurantName} has been placed for ${totalPrice}
-      </Text>
-      <LottieView
-        style={{ height: 200, alignSelf: "center" }}
-        source={require("../assets/animations/cooking.json")}
-        autoPlay
-        speed={0.75}
-      />
+      <View style={{ margin: 15, alignItems: "center", height: "100%" }}>
+        <LottieView
+          style={{ alignSelf: "center", marginBottom: 20 }}
+          source={require("../assets/animations/check-mark.json")}
+          autoPlay
+          autoSize={true}
+          speed={0.5}
+          loop={true}
+        />
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+          Your order for {restaurantName} has been placed for ${totalPrice}
+        </Text>
+        <ScrollView>
+          <MenuItems testFoods={lastOrder.items} hideCheckbox={true} />
+
+          <LottieView
+            style={{ height: 200, alignSelf: "center", marginBottom: 25 }}
+            source={require("../assets/animations/cooking.json")}
+            autoPlay
+            speed={0.75}
+            autoSize={true}
+          />
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
